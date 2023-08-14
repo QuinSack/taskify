11cb73fd39d375018eda95f30c8a6ddc4f3f6427
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-08-14 23:09:03 +0000

    added functionality for adding new item to list

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './styles.css'
 
-const InputField: React.FC = () => {
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField = ({todo, setTodo, handleAdd}: Props) => {
   return (
     <form className='input'>
-        <input type='input' placeholder='Enter a task' className='input__box' />
-        <button className='input__submit' type='submit'>Go</button>
+        <input type='input' placeholder='Enter a task' className='input__box' value={todo} onChange={(e)=>setTodo(e.target.value)} />
+        <button className='input__submit' type='submit' onClick={(e)=>handleAdd(e)}>Go</button>
     </form>
   )
 }
