0194e682f38b7c718121c55a92481c7e29527bce
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-08-15 23:28:35 +0000

    display tasks

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,26 @@ import './App.css';
 import { type } from 'os';
 import InputField from './components/InputField';
 import { Todo } from './model';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
   
   const [todo, setTodo] = useState<string>("");
-  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
     if(todo){
-      setTodoList([...todoList, {id: Date.now(), todo: todo, isDone: false}]);
+      setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}]);
       setTodo("");
     }
   }
-  console.log(todoList);
+  console.log(todos);
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
       <InputField todo={todo} setTodo = {setTodo} handleAdd = {handleAdd} />
-      
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
